185b15fa1f82431bface3713763d104206aa9ac7
Frederik Finckh <frederik.finckh@gmx.de>
2024-06-14 14:21:38 +0200

    add nvim tree

--- init.lua
+++ init.lua
@@ -259,6 +259,17 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      {
+        'nvim-tree/nvim-tree.lua',
+        version = '*',
+        lazy = false,
+        dependencies = {
+          'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+          require('nvim-tree').setup {}
+        end,
+      },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
