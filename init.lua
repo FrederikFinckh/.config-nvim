4b76010cef1968beb8afbec9b70d430da7df161a
Frederik Finckh <frederik.finckh@tngtech.com>
2026-06-26 20:59:13 +0200

    let me own this from scratch

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ' '
+vim.opt.nu = true -- Enable line numbers
+vim.opt.relativenumber = true -- Enable relative line numbers (super useful for jumping!)
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of indentation
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smartly indent a new line
+
+-- A basic keymap to save the file
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
